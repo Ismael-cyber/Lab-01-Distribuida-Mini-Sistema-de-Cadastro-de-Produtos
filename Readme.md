ab26d621761ded33c43b320f6d496ed3a53a92c9
is7tech <is7solucoestech@gmail.com>
2026-02-27 17:19:00 -0300

    Update in main.c and Readme.md

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 # Atividade Lab 01 - Mini Sistema de Cadastro de Produtos
+
+**Objetivo da Atividade:**Criar um Sistema de Cadastro de Produtos para revisar os conceitos de ponteiros e alocação de memória na linguagem C.
+
--- main.c
+++ main.c
@@ -1,47 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Tipo do dado que será usado
 typedef struct {
     int codigo;
-    char *nome[50];
+    char *nome;
     float preco;
     int quantidade;
 } Produto;
 
-// Espaço para armazenar os produtos
-Produto *produtos;
+typedef struct {
+    Produto *dados;
+    int quantidade;
+} Lista;
 
 //Funções do nosso sistema de cadastro de produtos
-void adicionar_produto(Produto *produtos) {
-    produtos = malloc(sizeof(Produto));
+void adicionar_produto(Lista *lista) {    
+    printf("Quantos produtos ira adicionar: ");
+    scanf("%d", &lista->quantidade);
 
-    if(produtos == NULL) {
+    lista->dados = malloc(lista->quantidade * sizeof(Produto));
+    
+    if(lista->dados == NULL) {
         printf("Erro: Falha na alocação de memória!\n");
     }
+    
+    for(int i = 0; i < lista->quantidade; i++) {
+        lista->dados[i].nome = malloc(50);
 
-    int qtd;
-    printf("Quantos produtos ira adicionar: ");
-    scanf("%d", &qtd);
-
-    for(int i = 0; i < qtd; i++) {
         printf("Nome: ");
-        scanf("%s", produtos->nome);
+        scanf("%49s", lista->dados[i].nome);
         printf("Preco: ");
-        scanf("%f", &produtos->preco);
+        scanf("%f", &lista->dados[i].preco);
         printf("Quantidade: ");
-        scanf("%d", &produtos->quantidade);
-        produtos->codigo++;
-        free(produtos);
+        scanf("%d", &lista->dados[i].quantidade);
+        lista->dados[i].codigo = i + 1;
     }
 };
-// void listar_produtos();
-// Produto* buscar_produto();
+
+void listar_produtos(Lista *lista) {
+    if(lista->dados == NULL) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    printf("+--------------------------------------------+\n");
+    printf("----------- LISTAGEM DOS PRODUTOS ------------\n");
+    printf("+--------------------------------------------+\n");
+    for(int i = 0; i < lista->quantidade; i++) {
+        printf("|  %3d  |  %-10s  |  %8.2f  |  %4d  |\n",
+            lista->dados[i].codigo, 
+            lista->dados[i].nome, 
+            lista->dados[i].preco, 
+            lista->dados[i].quantidade); 
+        }
+    printf("+--------------------------------------------+\n");
+};
+
+void buscar_produto(Lista *lista) {
+    char nome_produto[50];
+    printf("Qual produto esta buscando em nosso sistema: ");
+    scanf("%49s", nome_produto);
+
+    for(int i = 0; i < lista->quantidade; i++) {
+        if(strcmp(nome_produto, lista->dados[i].nome) == 0) {
+            printf("\nProduto encontrado: \n");
+            printf("|  %3d  |  %-10s  |  %8.2f  |  %4d  |\n",
+                lista->dados[i].codigo, 
+                lista->dados[i].nome, 
+                lista->dados[i].preco, 
+                lista->dados[i].quantidade);
+            break; 
+        }
+        else {
+            printf("Nenhum produto com este nome foi encontrado.\n");
+            break;
+        }
+    }
+};
 // void atualizar_estoque();
 // void remover_produto();
 // void liberar_memoria();
 
 int main(void) {
+    // Declarar vetor de Produtos
+    Lista lista;
+    lista.dados = NULL;
+    lista.quantidade = 0;
+
     //Loop para rodar sistema enquanto usuário não quer sair do programa
     int opcao;
     while(opcao != 6) {
@@ -63,16 +110,16 @@ int main(void) {
 
         //Condição 1
         if(opcao == 1) {
-            adicionar_produto(produtos);
+            adicionar_produto(&lista);
         }
         // //Condição 2
-        // if(opcao == 2) {
-        //     listar_produtos();
-        // }
-        // //Condição 3
-        // if(opcao == 3) {
-        //     buscar_produto();
-        // }
+        if(opcao == 2) {
+            listar_produtos(&lista);
+        }
+        //Condição 3
+        if(opcao == 3) {
+            buscar_produto(&lista);
+        }
         // //Condição 4
         // if(opcao == 4) {
         //     atualizar_estoque();
@@ -81,11 +128,11 @@ int main(void) {
         // if(opcao == 5) {
         //     remover_produto();
         // }
-        // //Condição 6
-        // if(opcao == 6) {
-        //     printf("Até Breve!");
-        // }
+        //Condição 6
+        if(opcao == 6) {
+            printf("VOCE FOI DESCONECTADO!");
+            exit;
+        }
     }
-    
     return 0;
 }
